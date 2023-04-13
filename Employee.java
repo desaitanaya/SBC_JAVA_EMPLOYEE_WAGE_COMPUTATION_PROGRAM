438b5b65d3bae4c153a942695aca442a2ae681fd
Tanaya <desaitanaya17@gmail.com>
2023-04-13 10:02:07 +0530

    [Tanaya] Add : Java program to calculate the Daily Wage of an Employee

--- Employee.java
+++ Employee.java
@@ -9,15 +9,24 @@ public class Employee {
 
 		// Used Math.random()*2 to get two values i.e., 0 and 1
 		int attendance = (int) (Math.random() * 2);
+		
+		// Constant variables
+		final int full_day_hour = 8;
+		final int wage_per_hour = 20;
+
+		// Initializing the variable
+		int salary = 0;
 
 		// For checking if Employee is present or not
 		if (attendance == 1) {
-			System.out.println("Employee is Present");
-		} else {
-			System.out.println("Employee is Absent");
+			// Calculating the salary is Employee is Present
+			salary = full_day_hour * wage_per_hour;
+			System.out.println("Employee is Present for Full time and the Salary is : " + salary);
+		} 
+		else {
+			System.out.println("Employee is Absent and the Salary is : " + salary);
 		}
 
 	}
 
 }
-
