c558c84962e6394bc077bc329e4350a180704da6
Tanaya <desaitanaya17@gmail.com>
2023-04-13 09:49:20 +0530

    [Tanaya] Add : Java program to check the attendance of Employee

--- Employee.java
+++ Employee.java
@@ -7,8 +7,10 @@ public class Employee {
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
 		System.out.println("----------------------------------------------");
 
+		// Used Math.random()*2 to get two values i.e., 0 and 1
 		int attendance = (int) (Math.random() * 2);
 
+		// For checking if Employee is present or not
 		if (attendance == 1) {
 			System.out.println("Employee is Present");
 		} else {
